c1fe5bca0c65f06d41fa78f5b8818cee3805f82b
will <will@panacea-software.com>
2018-06-11 15:16:19 +0100

    Added Inspina Theme

---
+++ Panacea.Communcation.Management.UI/Panacea.Communcation.Management.UI/Controllers/Insignia/LayoutsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Panacea.Communcation.Management.UI.Controllers
+{
+    public class LayoutsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OffCanvas()
+        {
+            return View();
+        }
+	}
+}
