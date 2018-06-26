6eb273f1e6ec04d1a5718f4182fe22b018bd996e
will <will@panacea-software.com>
2018-06-26 16:42:50 +0100

    Saving Groups

--- Panacea.Communcation.Management.UI/Panacea.Communcation.Management.UI/Models/GroupViewModels.cs
+++ Panacea.Communcation.Management.UI/Panacea.Communcation.Management.UI/Models/GroupViewModels.cs
@@ -19,22 +19,31 @@ namespace Panacea.Communcation.Management.UI.Models
     }
 
     public class AddGroupVM
-    {     
-        //public List<SelectListItem> ContactsList;
-        //public List<SelectListItem> OrganisationList;
-        //public List<ContactsGridItemVM> SelectedContacts;
-
+    {
         public AddGroupVM()
         {
-            //ContactsList = Helpers.DownDownListHelper.GetContactsSelectList();
-            //OrganisationList = Helpers.DownDownListHelper.GetOrganisationSelectList();
-            //SelectedContacts = new List<ContactsGridItemVM>();
+            Contacts = new List<SelectedContact>();
         }
 
-        public int Id { get; set; }
+        [Required]
         [Display(Name = "Group Name")]
         public string Name { get; set; }
+
+        [Required]
         [Display(Name = "Group Description")]
         public string Description { get; set; }
+
+        public List<SelectedContact> Contacts { get; set; }
     }
+
+    public class SelectedContact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+
+        public string Organisation { get; set; }
+    }
+
+
 }
